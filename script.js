5a3c0b8912b45e450fe9129eb0f6787703538eb0
kurd5083 <xxxnikitaxxxkyrdiashko@mail.ru>
2023-06-05 18:42:16 +0300

    first commit

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ function clock(){
     const hoursArrow  = document.querySelector('.hours')
     const minutsArrow = document.querySelector('.minuts')
     const secondsArrow = document.querySelector('.seconds')
-    const hourTime  = document.querySelector('.hourTime')
-    const minutTime = document.querySelector('.minutTime')
-    const secondTime = document.querySelector('.secondTime')
 
     const deg = 6
     setInterval(() => {
@@ -12,17 +9,10 @@ function clock(){
         const hours = day.getHours() *30
         const minuts = day.getMinutes() * deg
         const seconds = day.getSeconds() * deg
-        const hoursTime = day.getHours() 
-        const minutsTime = day.getMinutes() 
-        const secondsTime = day.getSeconds()
-
 
         hoursArrow.style.transform  = `rotateZ(${hours + (minuts / 12)}deg)`
         minutsArrow.style.transform = `rotateZ(${minuts}deg)`
         secondsArrow.style.transform = `rotateZ(${seconds}deg)`
-        hourTime.innerHTML = hoursTime;
-        minutTime.innerHTML = minutsTime;
-        secondTime.innerHTML = secondsTime;
     }, 0)
 
 }
